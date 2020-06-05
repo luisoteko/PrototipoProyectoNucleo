d1180bdc6cc5fb85e05596e267aca8329e56def7
Luis Becerra <luisoteko100@gmail.com>
2020-06-05 00:32:30 -0500

    little changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const Home = () => {
       <Layout pageName="Inicio">
         <form noValidate={false} autoComplete="on" className="form">
           <Link href="/Login"><Button variant="contained" color="primary">Iniciar Sesion</Button></Link>
-          <Link href="/Regitro"><Button variant="contained" color="primary">Registrame</Button></Link>
+          <Link href="/Registro"><Button variant="contained" color="primary">Registrame</Button></Link>
           <Link href="/Conocenos"><Button variant="contained" color="primary">Conocenos</Button></Link>
         </form>
       </Layout>
